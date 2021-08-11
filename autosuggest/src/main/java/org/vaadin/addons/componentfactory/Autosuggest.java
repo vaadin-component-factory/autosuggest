6b214e9d78e71a33ce20760d0ab991ab6c8c0923
Unknown <unknown@example.com>
2021-08-11 12:44:59 +0100

    fix: fe component version

--- autosuggest/src/main/java/org/vaadin/addons/componentfactory/Autosuggest.java
+++ autosuggest/src/main/java/org/vaadin/addons/componentfactory/Autosuggest.java
@@ -52,7 +52,7 @@ import java.util.stream.Collectors;
  */
 
 @Tag("vcf-autosuggest")
-@NpmPackage(value = "@vaadin-component-factory/vcf-autosuggest", version = "1.1.1")
+@NpmPackage(value = "@vaadin-component-factory/vcf-autosuggest", version = "1.1.2")
 @JsModule("@vaadin-component-factory/vcf-autosuggest/src/vcf-autosuggest.js")
 //@JsModule("./vcf-autosuggest.js")
 @CssImport(value = "@vaadin-component-factory/vcf-autosuggest/styles/style.css")
